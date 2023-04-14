e8ec1c74716f973f64a4e2504ed28caebdf56d9f
Jeremy Little <jeremy@jwlittle.net>
2023-04-13 21:46:41 -0500

    implemented MC.UI.Core.MVVM as a re-usable project

---
+++ src/MC.UI.Core/MC.UI.Core.MVVM/ServiceCollectionBuilder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using CommunityToolkit.Mvvm.DependencyInjection;
+
+
+namespace MC.UI.Core.MVVM {
+  public class ServiceCollectionBuilder {
+    private readonly IServiceCollection _serviceCollection;
+
+    public ServiceCollectionBuilder() {
+      _serviceCollection = new ServiceCollection();
+    }
+
+    public ServiceCollectionBuilder ConfigureServices(Action<IServiceCollection> action) {
+      action.Invoke(_serviceCollection);
+      return this;
+    }
+
+    public ServiceCollectionBuilder AddViewModels() {
+      var viewModels = AppDomain.CurrentDomain.GetAssemblies()
+        .SelectMany(assembly => assembly.GetTypes())
+        .Where(type => type.IsSubclassOf(typeof(ViewModelBase)));
+
+      foreach (var viewModel in viewModels) {
+        _serviceCollection.AddTransient(viewModel);
+      }
+
+      return this;
+    }
+
+    public IServiceProvider Build() {
+      var serviceProvider = _serviceCollection.BuildServiceProvider();
+      Ioc.Default.ConfigureServices(serviceProvider);
+
+      return serviceProvider;
+    }
+
+    public ServiceCollectionBuilder AddWindow<T>() where T : class {
+      _serviceCollection.AddTransient<T>();
+
+      return this;
+    }
+
+    public ServiceCollectionBuilder AddConfiguration(string fileName = "appsettings.json") {
+      if (File.Exists(fileName) == false) {
+        throw new FileNotFoundException($"Required file not found inMC.UI.Core.MVVM.ServiceCollectionBuilder.AddConfiguration", fileName);
+      }
+
+      var builder = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory).AddJsonFile(fileName, false, true);
+
+      _serviceCollection.AddSingleton(builder.Build() as IConfiguration);
+
+      return this;
+    }
+  }
+}
---
+++ src/MC.UI.Core/MC.UI.Core.MVVM/ViewModelBase.cs
@@ -0,0 +1,5 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MC.UI.Core.MVVM {
+  public abstract class ViewModelBase : ObservableObject { }
+}
