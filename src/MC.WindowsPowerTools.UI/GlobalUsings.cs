a96dab51e756a2c32b0b1ca5f8d35c896995980f
Jeremy Little <muqluk@yahoo.com>
2023-04-16 07:06:43 -0500

    working through relay command implementation changes since my last wpf project

---
+++ src/MC.WindowsPowerTools.UI/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Collections.ObjectModel;
+
+global using CommunityToolkit.Mvvm.Collections;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
--- src/MC.WindowsPowerTools.UI/Views/RegReader/RegReaderViewModel.cs
+++ src/MC.WindowsPowerTools.UI/Views/RegReader/RegReaderViewModel.cs
@@ -3,20 +3,26 @@
 using MC.UI.Core.MVVM;
 
 namespace MC.UI.WindowsPowerTools.Views.RegReader {
-  public class RegReaderViewModel : ViewModelBase {
+  public partial class RegReaderViewModel : ViewModelBase {
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(GetSubKeysCommand))]
+    private RegistryHive selectedHive = RegistryHive.CurrentUser;
 
-    private RegistryHive _selectedHive = RegistryHive.CurrentUser;
-    public RegistryHive SelectedHive {
-      get => _selectedHive;
-      set {
-        if (value != _selectedHive) {
-          _selectedHive = value;
-          OnPropertyChanged(nameof(SelectedHive));
+    [ObservableProperty]
+    private ObservableCollection<RegistryKey> subKeys = new ObservableCollection<RegistryKey>();
+
+    [RelayCommand]
+    private void GetSubKeys(RegistryHive hive) {
+      using (RegistryKey key = RegistryKey.OpenBaseKey(hive, RegistryView.Registry64)) {
+        SubKeys.Clear();
+        foreach (string subkeyName in key.GetSubKeyNames()) {
+          var derp = key.OpenSubKey(subkeyName);
+          SubKeys.Add(key.OpenSubKey(subkeyName));
         }
       }
     }
 
-    public RegReaderViewModel() { }
+
 
   }
 }
